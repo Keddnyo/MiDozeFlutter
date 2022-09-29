d70eae4f41b752887e083b3a365a7ed2b9bc5b48
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-09-29 05:25:30 +0300

    Update firmware.dart

--- lib/firmware.dart
+++ lib/firmware.dart
@@ -1,16 +1,40 @@
 class Firmware {
-  final String firmwareVersion;
-  final String firmwareUrl;
+  final String firmwareVersion,
+      firmwareUrl,
+      resourceVersion,
+      resourceUrl,
+      baseResourceVersion,
+      baseResourceUrl,
+      fontVersion,
+      fontUrl,
+      gpsVersion,
+      gpsUrl;
 
   Firmware({
     required this.firmwareVersion,
     required this.firmwareUrl,
+    required this.resourceVersion,
+    required this.resourceUrl,
+    required this.baseResourceVersion,
+    required this.baseResourceUrl,
+    required this.fontVersion,
+    required this.fontUrl,
+    required this.gpsVersion,
+    required this.gpsUrl,
   });
 
   factory Firmware.fromJson(Map<String, dynamic> json) {
     return Firmware(
       firmwareVersion: json['firmwareVersion'],
       firmwareUrl: json['firmwareUrl'],
+      resourceVersion: json['resourceVersion'],
+      resourceUrl: json['resourceUrl'],
+      baseResourceVersion: json['baseResourceVersion'],
+      baseResourceUrl: json['baseResourceUrl'],
+      fontVersion: json['fontVersion'],
+      fontUrl: json['fontUrl'],
+      gpsVersion: json['gpsVersion'],
+      gpsUrl: json['gpsUrl'],
     );
   }
 }
