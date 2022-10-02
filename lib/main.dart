358fbd84f4c09e36a6f22769a65dc49922bda300
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2022-10-02 15:48:27 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -35,10 +35,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-          bottom: Radius.circular(20),
-        )),
         title: Text(widget.title),
         actions: [
           IconButton(
@@ -57,7 +53,19 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: deviceList(),
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.deepPurple,
+              Colors.red,
+            ],
+          ),
+        ),
+        child: deviceList(),
+      ),
     );
   }
 
