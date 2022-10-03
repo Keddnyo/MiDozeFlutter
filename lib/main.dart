8958b0ad252ba70c64ae8b43d02ab02548df585d
Timur Zhdikhanov <65981689+Keddnyo@users.noreply.github.com>
2022-10-03 11:59:44 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String appBarTitle = 'MiDoze';
+  String appBarTitle = 'Apps';
 
   void changeTitle(String title) {
     setState(() {
