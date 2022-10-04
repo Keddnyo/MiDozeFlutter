8ff6c5ebd35be80e8d23ae08af47b501d1e84a56
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-10-04 19:32:18 +0300

    Update watchface.dart

--- lib/ui_elements/page_view/elements/watchface.dart
+++ lib/ui_elements/page_view/elements/watchface.dart
@@ -6,71 +6,79 @@ import '../../../remote/requests.dart';
 import 'package:http/http.dart' as http;
 
 Widget watchfaceList() {
-  // return FutureBuilder(
-  //   future: anything(),
-  //   builder: ((context, snapshot) {
-  //     return Text(snapshot.data.toString());
-  //   }),
-  // );
-  return FutureBuilder<watchface_remote.Watchface>(
-    future: getWatchfaceData('hmpace.watch.v7'),
+  return FutureBuilder(
+    future: anything(),
     builder: ((context, snapshot) {
-      if (snapshot.hasData) {
-        var response = jsonDecode(snapshot.data.toString());
-        var watchface = watchface_remote.Watchface.fromJson(
-            response as Map<String, dynamic>);
+      if (snapshot.data != null) {
+        var response = snapshot.data.toString();
+        var json = jsonDecode(response);
+        var title = watchface_remote.Watchface.fromJson(json).title;
 
-        final count = watchface.length;
-
-        GridView.builder(
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 200,
-          ),
-          itemCount: count,
-          itemBuilder: (context, index) {
-            return Card(
-              elevation: 10,
-              shape: const RoundedRectangleBorder(
-                side: BorderSide(
-                  color: Colors.black,
-                  width: 1,
-                ),
-                borderRadius: BorderRadius.all(
-                  Radius.circular(15),
-                ),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(5),
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Image.network(watchface.preview),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(5),
-                      child: Text(
-                        watchface.title,
-                        style: const TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          },
-        );
-      } else if (snapshot.hasError) {
-        return Text('${snapshot.error}');
+        return Text(title);
+      } else {
+        return const Text('null');
       }
-      return const CircularProgressIndicator();
     }),
   );
+  // return FutureBuilder<watchface_remote.Watchface>(
+  //   future: getWatchfaceData('hmpace.watch.v7'),
+  //   builder: ((context, snapshot) {
+  //     if (snapshot.hasData) {
+  //       var response = jsonDecode(snapshot.data.toString());
+  //       var watchface = watchface_remote.Watchface.fromJson(
+  //           response as Map<String, dynamic>);
+
+  //       final count = watchface.length;
+
+  //       GridView.builder(
+  //         gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+  //           maxCrossAxisExtent: 200,
+  //         ),
+  //         itemCount: count,
+  //         itemBuilder: (context, index) {
+  //           return Card(
+  //             elevation: 10,
+  //             shape: const RoundedRectangleBorder(
+  //               side: BorderSide(
+  //                 color: Colors.black,
+  //                 width: 1,
+  //               ),
+  //               borderRadius: BorderRadius.all(
+  //                 Radius.circular(15),
+  //               ),
+  //             ),
+  //             child: Padding(
+  //               padding: const EdgeInsets.all(5),
+  //               child: Column(
+  //                 children: [
+  //                   Expanded(
+  //                     child: Image.network(watchface.preview),
+  //                   ),
+  //                   Padding(
+  //                     padding: const EdgeInsets.all(5),
+  //                     child: Text(
+  //                       watchface.title,
+  //                       style: const TextStyle(
+  //                           fontSize: 14, fontWeight: FontWeight.bold),
+  //                       textAlign: TextAlign.center,
+  //                     ),
+  //                   ),
+  //                 ],
+  //               ),
+  //             ),
+  //           );
+  //         },
+  //       );
+  //     } else if (snapshot.hasError) {
+  //       return Text('${snapshot.error}');
+  //     }
+  //     return const CircularProgressIndicator();
+  //   }),
+  // );
 }
 
-Future<String> anything() {
-  return http.read(
+Future<String> anything() async {
+  return await http.read(
     Uri.https(
       'cors-anywhere.herokuapp.com',
       'https://watch-appstore.iot.mi.com/api/watchface/prize/tabs',
