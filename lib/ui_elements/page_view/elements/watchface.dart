9c570fe100277794b2cef6f324093b7f84329d64
Keddnyo <65981689+Keddnyo@users.noreply.github.com>
2022-10-06 00:36:01 +0300

    New

--- lib/ui_elements/page_view/elements/watchface.dart
+++ lib/ui_elements/page_view/elements/watchface.dart
@@ -5,26 +5,15 @@ import '../../../data_models/remote/watchface.dart' as watchface_remote;
 import 'package:http/http.dart' as http;
 
 Widget watchfaceList() {
-  return FutureBuilder(
+  return FutureBuilder<List<watchface_remote.Watchface>>(
     future: anything(),
     builder: ((context, snapshot) {
-      if (snapshot.data == null) {
-        return const Text('Error');
-      } else {
-        var response = snapshot.data.toString();
-        var json = jsonDecode(response);
-        var watchface =
-            watchface_remote.Watchface.fromJson(json as Map<String, dynamic>);
-        var title = watchface.title;
-        var preview = watchface.preview;
-        var url = watchface.url;
-        var length = watchface.length;
-
+      if (snapshot.data != null) {
         return GridView.builder(
           gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
             maxCrossAxisExtent: 200,
           ),
-          itemCount: length,
+          itemCount: snapshot.data!.length,
           itemBuilder: (context, index) {
             return Card(
               elevation: 10,
@@ -42,12 +31,12 @@ Widget watchfaceList() {
                 child: Column(
                   children: [
                     Expanded(
-                      child: Image.network(preview),
+                      child: Image.network(snapshot.data![index].preview),
                     ),
                     Padding(
                       padding: const EdgeInsets.all(5),
                       child: Text(
-                        title,
+                        snapshot.data![index].title,
                         style: const TextStyle(
                             fontSize: 14, fontWeight: FontWeight.bold),
                         textAlign: TextAlign.center,
@@ -59,14 +48,16 @@ Widget watchfaceList() {
             );
           },
         );
+      } else if (snapshot.hasError) {
+        return Text('${snapshot.error}');
       }
       return const CircularProgressIndicator();
     }),
   );
 }
 
-Future<String> anything() async {
-  return await http.read(
+Future<List<watchface_remote.Watchface>> anything() async {
+  var response = await http.read(
     Uri.https(
       'cors-anywhere.herokuapp.com',
       'https://watch-appstore.iot.mi.com/api/watchface/prize/tabs',
@@ -75,9 +66,29 @@ Future<String> anything() async {
       },
     ),
     headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
-      'Accept': '*/*'
+      'Watch-Appstore-Common': '_locale=us&_language=en',
+      // 'Access-Control-Allow-Origin': '*',
+      // 'Content-Type': 'application/json',
+      // 'Accept': '*/*'
     },
   );
+
+  var json = jsonDecode(response) as Map<String, dynamic>;
+
+  List<watchface_remote.Watchface> watchfaceArray = [];
+
+  for (var d = 0; d < json.length; d++) {
+    var watchfaceList = json['data'][d]['list'] as List<dynamic>;
+
+    for (var w = 0; w < watchfaceList.length; w++) {
+      var watchface = watchfaceList[w] as Map<String, dynamic>;
+
+      watchfaceArray.add(watchface_remote.Watchface(
+          title: watchface['display_name'],
+          preview: watchface['icon'],
+          url: watchface['config_file']));
+    }
+  }
+
+  return watchfaceArray;
 }
